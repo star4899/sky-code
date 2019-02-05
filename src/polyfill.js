6bbf8d1b234e7ed7eda8add8564a2948ea85a9ad
skydown <star4899@naver.com>
2019-02-05 21:33:19 +0900

    foreach ie polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,10 +1,9 @@
-module.exports.forEach = () => {
-	if(window.NodeList && !NodeList.prototype.forEach){
-		NodeList.prototype.forEach = (callback, thisArg) => {
-			thisArg = thisArg || window;
-			for(var i = 0; i < this.length; i++){
-				callback.call(thisArg, this[i], i, this);
-			};
+// ie polyfill
+if(window.NodeList && !NodeList.prototype.forEach){
+	NodeList.prototype.forEach = function(callback, thisArg){
+		thisArg = thisArg || window;
+		for(var i = 0; i < this.length; i++){
+			callback.call(thisArg, this[i], i, this);
 		};
-	}
+	};
 };
